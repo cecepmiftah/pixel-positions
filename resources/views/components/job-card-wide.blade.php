899384af47534d0c019b56776712d5815c422941
Cecep Miftah Solahudin <cecepmiftah73@gmail.com>
2025-02-25 17:33:15 +0700

    Implement Employer and Job models, controllers, and related views; add seeder and factory for data generation; update tests and configuration.

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,12 +1,12 @@
-<div class="p-4 bg-white/5 rounded-xl flex gap-x-6">
+<x-panel class="flex gap-x-6">
     <div>
-        <img src="https://picsum.photos/seed/{{ rand(0, 100000) }}/100/100" alt="" class="rounded-xl">
+        <x-employer-logo />
     </div>
 
-    <div class="flex-1">
+    <div class="flex-1 flex flex-col">
         <a href="#" class="self-start text-sm text-gray-400">Laracast</a>
-        <h3>Video Producer</h3>
-        <p>Full Time - From $60,000</p>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">Video Producer</h3>
+        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
     </div>
 
     <div>
@@ -15,4 +15,4 @@
         <x-tag>Tag</x-tag>
     </div>
 
-</div>
+</x-panel>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,12 +13,13 @@ export default {
         extend: {
             fontFamily: {
                 sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+                "hanken-grotesk": ["Hanken Grotesk", "sans-serif"],
             },
             colors: {
                 black: "#060606",
             },
-            fontFamily: {
-                "hanken-grotesk": ["Hanken Grotesk", "sans-serif"],
+            fontSize: {
+                "2xs": ".625rem", //10px
             },
         },
     },
